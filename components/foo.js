e2df56e2d88e01eac2f89ea3501e946583fc25fc
AD001\z0043xbv <andreea.pavel@siemens.com>
2021-01-09 00:03:29 +0200

    Functional version

--- components/foo.js
+++ components/foo.js
@@ -1,16 +1,16 @@
 // Variables declarations
-let city = 'Brasov';
+var apiLink = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiKey = "&appid=00caa9bdfcc6f16336db1ec60c59f314";
+var cityName = '';
 
 AFRAME.registerComponent("foo", {
-
-
-   
     init: function() {
       let self = this.el;
       let sky = document.querySelector('#sky');
 
       this.el.addEventListener("click", (e)=>{
-        axios.get(link)
+        var url = apiLink + cityName + apiKey;
+        axios.get(url)
           .then(function (response) {
             const temperature = Math.round((response.data.main.temp - 273.15) * 10) / 10;
             self.children[0].setAttribute("value", temperature + ' °C');
@@ -33,39 +33,15 @@ AFRAME.registerComponent("foo", {
     },
 
     updateValues: function(cityName) {
-      console.log("Cityname before updateValue: " + this.cityName);
+      // console.log("Cityname before updateValue: " + this.cityName);
       let temperatureShow = document.querySelector('#temperaturePlaneText');
       temperatureShow.setAttribute("value", "What's the temperature in " + cityName + "?");
       this.cityName = cityName;
-      console.log("Cityname after updateValue: " + this.cityName);
+      // console.log("Cityname after updateValue: " + this.cityName);
     }
   });
 
-
-
 window.toggleAction =   function getTemperatureByLocation(cityName) {
-  
-  let link = 'http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=00caa9bdfcc6f16336db1ec60c59f314';
-  let sky = document.querySelector('#sky');
-  let foo = document.querySelector("#foo")
-
-    axios.get(link)
-      .then(function (response) {
-        const temperature = Math.round((response.data.main.temp - 273.15) * 10) / 10;
-        foo.children[0].setAttribute("value", temperature + ' °C');
-        if(temperature > 20) {
-          sky.setAttribute('color', 'yellow');
-          sky.setAttribute('src', '#cubes');
-        }
-        else {
-          sky.setAttribute('color', '');
-          sky.setAttribute('src', '#coldTexture');
-        }
-      })
-      .catch(function (error) {
-        self.children[0].setAttribute("value", error);
-        console.log(error);
-      })
-      .then(function () {
-      });
+  let updateValuesFunction = document.querySelector("#foo").components.foo.__proto__.updateValues;
+  updateValuesFunction(cityName);
 }
