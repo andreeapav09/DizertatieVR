27f23fe18fc9aa09a9f23f35f28aa25aee1a8d1c
AD001\z0043xbv <andreea.pavel@siemens.com>
2021-01-08 23:24:49 +0200

    something that works at least

--- components/foo.js
+++ components/foo.js
@@ -1,17 +1,26 @@
+// Variables declarations
+let city = 'Brasov';
+
 AFRAME.registerComponent("foo", {
+
+
+   
     init: function() {
       let self = this.el;
       let sky = document.querySelector('#sky');
+
       this.el.addEventListener("click", (e)=>{
-        axios.get('http://api.openweathermap.org/data/2.5/weather?q=Mecca&appid=00caa9bdfcc6f16336db1ec60c59f314')
+        axios.get(link)
           .then(function (response) {
             const temperature = Math.round((response.data.main.temp - 273.15) * 10) / 10;
-            self.children[0].setAttribute("value", temperature);
+            self.children[0].setAttribute("value", temperature + ' °C');
             if(temperature > 20) {
-              sky.setAttribute('color', 'red');
+              sky.setAttribute('color', 'yellow');
+              sky.setAttribute('src', '#cubes');
             }
             else {
-              sky.setAttribute('color', 'blue');
+              sky.setAttribute('color', '');
+              sky.setAttribute('src', '#coldTexture');
             }
           })
           .catch(function (error) {
@@ -21,5 +30,42 @@ AFRAME.registerComponent("foo", {
           .then(function () {
           });
       })
+    },
+
+    updateValues: function(cityName) {
+      console.log("Cityname before updateValue: " + this.cityName);
+      let temperatureShow = document.querySelector('#temperaturePlaneText');
+      temperatureShow.setAttribute("value", "What's the temperature in " + cityName + "?");
+      this.cityName = cityName;
+      console.log("Cityname after updateValue: " + this.cityName);
     }
-  })
+  });
+
+
+
+window.toggleAction =   function getTemperatureByLocation(cityName) {
+  
+  let link = 'http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=00caa9bdfcc6f16336db1ec60c59f314';
+  let sky = document.querySelector('#sky');
+  let foo = document.querySelector("#foo")
+
+    axios.get(link)
+      .then(function (response) {
+        const temperature = Math.round((response.data.main.temp - 273.15) * 10) / 10;
+        foo.children[0].setAttribute("value", temperature + ' °C');
+        if(temperature > 20) {
+          sky.setAttribute('color', 'yellow');
+          sky.setAttribute('src', '#cubes');
+        }
+        else {
+          sky.setAttribute('color', '');
+          sky.setAttribute('src', '#coldTexture');
+        }
+      })
+      .catch(function (error) {
+        self.children[0].setAttribute("value", error);
+        console.log(error);
+      })
+      .then(function () {
+      });
+}
