98d09f879490583fa1b2e68f0c36dc3cf8f55868
AD001\z0043xbv <andreea.pavel@siemens.com>
2021-01-08 18:22:30 +0200

    Make sky to change based on the temperature

--- components/foo.js
+++ components/foo.js
@@ -1,19 +1,24 @@
 AFRAME.registerComponent("foo", {
     init: function() {
-      let self = this.el
+      let self = this.el;
+      let sky = document.querySelector('#sky');
       this.el.addEventListener("click", (e)=>{
-        // Make a request for a user with a given ID
-        axios.get('http://api.openweathermap.org/data/2.5/weather?q=Brasov&appid=00caa9bdfcc6f16336db1ec60c59f314')
+        axios.get('http://api.openweathermap.org/data/2.5/weather?q=Mecca&appid=00caa9bdfcc6f16336db1ec60c59f314')
           .then(function (response) {
-            self.children[0].setAttribute("value", response.data.main.temp);
-            // console.log(response);
+            const temperature = Math.round((response.data.main.temp - 273.15) * 10) / 10;
+            self.children[0].setAttribute("value", temperature);
+            if(temperature > 20) {
+              sky.setAttribute('color', 'red');
+            }
+            else {
+              sky.setAttribute('color', 'blue');
+            }
           })
           .catch(function (error) {
             self.children[0].setAttribute("value", error);
-            // console.log(error);
+            console.log(error);
           })
           .then(function () {
-            // always executed
           });
       })
     }
